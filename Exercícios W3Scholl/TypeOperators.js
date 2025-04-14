2b7bc49d9668dd35139d031f367f42131718d9f1
Vinicius Santos <165097853+RosiestSloth@users.noreply.github.com>
2025-04-14 08:19:48 -0300

    Create TypeOperators.js

---
+++ Exercícios W3Scholl/TypeOperators.js
@@ -0,0 +1,13 @@
+const x = new Number(10); //
+
+let y = new String("10");
+
+console.log(typeof(x));
+
+console.log(typeof(y));
+
+console.log("Aqui, vou retornar o instanceof");
+
+console.log(x instanceof Number);
+
+console.log(y instanceof String);
